5de0e2ec03b0ad22870ad4f3d060a72fb5c93582
gaozaici <gaozaici@qq.com>
2022-08-25 21:28:47 +0800

    update

--- Template/Plugin.cpp
+++ Template/Plugin.cpp
@@ -37,21 +37,12 @@
 #include "Global.h"
 #include "MC/VanillaItemNames.hpp"
 #include "MC/EnchantUtils.hpp"
+#include "MC/ListTag.hpp"
 #include <LLAPI.h>
 #include <ServerAPI.h>
+#include "VftableHook.h"
 
 using namespace std;
-
-Logger logger(PLUGIN_NAME);
-
-
-class HashedString const &EntityCanonicalName(enum ActorType);
-
-extern enum ActorType EntityTypeFromString(std::string const &);
-
-inline void CheckProtocolVersion() {
-    logger.info("插件开始加载");
-}
 
 #define KILL_MOB_COUNT "kill_mob_count"
 #define KILL_BOSS_COUNT "kill_boss_count"
@@ -61,10 +52,59 @@ inline void CheckProtocolVersion() {
 #define PLAYER_EAT_COUNT "player_eat_count"
 #define PLAYER_ATTACK_COUNT "player_attack_count"
 
+enum EnchantType : short {
+    arrowDamage = 19,//力量
+    arrowFire = 21,//火矢
+    arrowInfinite = 22,//无限
+    arrowKnockback = 20,//冲击
+    crossbowMultishot = 33,//多重箭
+    crossbowPiercing = 34,//穿透
+    crossbowQuickCharge = 35,//快速装填
+    curse_binding = 0,//绑定诅咒
+    curse_vanishing = 0,//消失诅咒
+    damage_all = 9,//锋利
+    damage_arthropods = 11,//节肢克星
+    damage_undead = 10,//亡灵克星
+    digging = 15,//效率
+    durability = 17,//耐久
+    fire = 13,//火焰附加
+    fishingSpeed = 24,//饵钓
+    frostwalker = 25,//冰霜行者
+    knockback = 12,//击退
+    lootBonus = 14,//抢夺
+    lootBonusDigger = 18,//时运
+    lootBonusFishing = 23,//海之眷顾
+    mending = 26,//经验修补
+    oxygen = 6,//水下呼吸
+    protect_all = 0,//保护
+    protect_explosion = 3,//爆炸保护
+    protect_fall = 2,//摔落保护
+    protect_fire = 1,//火焰保护
+    protect_projectile = 4,//弹射物保护
+    thorns = 5,//荆棘
+    untouching = 16,//精准采集
+    waterWalker = 7,//深海探索者
+    waterWorker = 8,//水下速掘
+    tridentChanneling = 32,//引雷
+    tridentLoyalty = 31,//忠诚
+    tridentRiptide = 30,//激流
+    tridentImpaling = 29,//穿刺
+};
+
+Logger logger(PLUGIN_NAME);
+
+class HashedString const &EntityCanonicalName(enum ActorType);
+
+extern enum ActorType EntityTypeFromString(std::string const &);
+
+uintptr_t imageBaseAddr;
+
 void PluginInit() {
-    CheckProtocolVersion();
+    HMODULE handle = GetModuleHandle(nullptr);
+    logger.info("插件开始加载 BDS句柄:{}", (void *) handle);
+    imageBaseAddr = (uintptr_t) handle;
     Event::PlayerJoinEvent::subscribe_ref([](auto &event) {
-        event.mPlayer->sendText("§b欢迎玩家" + event.mPlayer->getName() + "进入游戏！");
+        event.mPlayer->sendText("§b欢迎玩家§e" + event.mPlayer->getName() + "§b进入游戏！");
         return true;
     });
     Event::RegCmdEvent::subscribe_ref([](auto &event) {
@@ -94,7 +134,7 @@ void PluginInit() {
                 itemStack.setCustomLore({"击败Boss奖励物品"});
                 itemStack.getUserData()->putBoolean("BossItem", true);
                 EnchantUtils::applyEnchant(itemStack, Enchant::Type::knockback, 5, false);
-                player->sendText("§e玩家" + CommandUtils::getActorName(*actor) + "击杀了Boss生物");
+                player->sendText("§e玩家§b" + CommandUtils::getActorName(*actor) + "§e击杀了Boss生物");
                 player->giveItem(&itemStack);
             }
         }
@@ -116,36 +156,175 @@ void PluginInit() {
         Scoreboard::addScore(event.mPlayer, PLAYER_ATTACK_COUNT, 1);
         return true;
     });
-    Event::MobSpawnEvent::subscribe_ref([](auto &event) {
-        return true;
-    });
 }
 
 TInstanceHook(Mob*, "?spawnMob@Spawner@@QEAAPEAVMob@@AEAVBlockSource@@AEBUActorDefinitionIdentifier@@PEAVActor@@AEBVVec3@@_N44@Z", Spawner, BlockSource &source, ActorDefinitionIdentifier const &identifier, Actor *entity, Vec3 const &pos, bool b1, bool b2, bool b3) {
     Mob *mob = original(this, source, identifier, entity, pos, b1, b2, b3);
-    if (mob && identifier.getCanonicalName() == "minecraft:creeper") {
-        if (mob->getRandom().nextInt(0,100) > 70){
-            mob->setNameTag("§e闪击Boss§r");
-            mob->addDefinitionGroup("minecraft:charged_creeper");
-            Level *level = dAccess<Level*,0>(this);
-            Player *player = level->getPlayer("VanessaSpy");
-            if (player){
-                //设置生物攻击目标
-                mob->setTarget(player);
-                if (player->distanceTo(*mob) < 50.0f){
-                    auto const &instance = entity->getAttribute(SharedAttributes::HEALTH);
-                    auto &p = (AttributeInstance &) instance;
-                    p.setMaxValue(40);
-                    p.setCurrentValue(40);
+    if (mob) {
+        string const &name = identifier.getCanonicalName();
+        if (name == "minecraft:creeper") {
+            if (mob->getRandom().nextInt(0, 100) > 70) {
+                mob->setNameTag("闪电苦力怕");
+                mob->addDefinitionGroup("minecraft:charged_creeper");
+                Level *level = dAccess<Level *, 0>(this);
+                Player *player = level->getPlayer("VanessaSpy");
+                if (player) {
+                    //设置生物攻击目标
+                    mob->setTarget(player);
+                    if (player->distanceTo(*mob) < 50.0f) {
+                        auto const &instance = entity->getAttribute(SharedAttributes::HEALTH);
+                        auto &p = (AttributeInstance &) instance;
+                        p.setMaxValue(40);
+                        p.setCurrentValue(40);
+                    }
                 }
+            }
+        } else if (name == "minecraft:zombie" || name == "minecraft:zombie_villager"|| name == "minecraft:husk") {
+            if (mob->getRandom().nextInt(0, 100) > 60) {
+                if (mob->getRandom().nextBoolean()) {
+                    mob->setNameTag("Grumm");
+                    if (mob->getRandom().nextInt(0, 100) > 50) {
+                        ItemStack itemStack("minecraft:diamond_sword", 1, 0, nullptr);
+                        unique_ptr<Tag> tag = Tag::newTag(Tag::Type::List);
+                        unique_ptr<ListTag> listTag = (unique_ptr<ListTag> &&) tag;
+                        {
+                            unique_ptr<Tag> t = Tag::newTag(Tag::Type::Compound);
+                            unique_ptr<CompoundTag> compoundTag = (unique_ptr<CompoundTag> &&) t;
+                            compoundTag->putShort("id", EnchantType::durability);
+                            compoundTag->putShort("lvl", -32767);
+                            listTag->add(std::move(compoundTag));
+                        }
+                        {
+                            unique_ptr<Tag> t = Tag::newTag(Tag::Type::Compound);
+                            unique_ptr<CompoundTag> compoundTag = (unique_ptr<CompoundTag> &&) t;
+                            compoundTag->putShort("id", EnchantType::damage_arthropods);
+                            compoundTag->putShort("lvl", -32767);
+                            listTag->add(std::move(compoundTag));
+                        }
+                        {
+                            unique_ptr<Tag> t = Tag::newTag(Tag::Type::Compound);
+                            unique_ptr<CompoundTag> compoundTag = (unique_ptr<CompoundTag> &&) t;
+                            compoundTag->putShort("id", EnchantType::damage_undead);
+                            compoundTag->putShort("lvl", -32767);
+                            listTag->add(std::move(compoundTag));
+                        }
+                        {
+                            unique_ptr<Tag> t = Tag::newTag(Tag::Type::Compound);
+                            unique_ptr<CompoundTag> compoundTag = (unique_ptr<CompoundTag> &&) t;
+                            compoundTag->putShort("id", EnchantType::mending);
+                            compoundTag->putShort("lvl", -32767);
+                            listTag->add(std::move(compoundTag));
+                        }
+                        if (itemStack.getUserData()) {
+                            itemStack.getUserData()->put(ItemStack::TAG_ENCHANTS, std::move(listTag));
+                        } else {
+                            unique_ptr<Tag> t = Tag::newTag(Tag::Type::Compound);
+                            unique_ptr<CompoundTag> compoundTag = (unique_ptr<CompoundTag> &&) t;
+                            compoundTag->put(ItemStack::TAG_ENCHANTS, std::move(listTag));
+                            itemStack.setUserData(std::move(compoundTag));
+                        }
+                        mob->setCarriedItem(itemStack);
+                    }
+                } else {
+                    mob->setNameTag("Dinnerbone");
+                    if (mob->getRandom().nextInt(0,100) > 70 ){
+                        mob->setArmor((ArmorSlot)0,ItemStack(VanillaItemNames::DiamondHelmet.getString(),1,0, nullptr));
+                        mob->setArmor((ArmorSlot)1,ItemStack(VanillaItemNames::DiamondChestplate.getString(),1,0, nullptr));
+                        mob->setArmor((ArmorSlot)2,ItemStack(VanillaItemNames::DiamondLeggings.getString(),1,0, nullptr));
+                        mob->setArmor((ArmorSlot)3,ItemStack(VanillaItemNames::DiamondBoots.getString(),1,0, nullptr));
+                        if (mob->getRandom().nextBoolean()){
+                            auto const &instance = entity->getAttribute(SharedAttributes::HEALTH);
+                            auto &p = (AttributeInstance &) instance;
+                            p.setMaxValue(50);
+                            p.setCurrentValue(50);
+                        }
+                    }
+                }
+            }
+        } else if (name == "minecraft:skeleton") {
+            if (mob->getRandom().nextInt(0, 100) > 90) {
+                ItemStack itemStack("minecraft:bow", 1, 0, nullptr);
+                unique_ptr<Tag> tag = Tag::newTag(Tag::Type::List);
+                unique_ptr<ListTag> listTag = (unique_ptr<ListTag> &&) tag;
+                {
+                    unique_ptr<Tag> t = Tag::newTag(Tag::Type::Compound);
+                    unique_ptr<CompoundTag> compoundTag = (unique_ptr<CompoundTag> &&) t;
+                    compoundTag->putShort("id", EnchantType::durability);
+                    compoundTag->putShort("lvl", -32767);
+                    listTag->add(std::move(compoundTag));
+                }
+                {
+                    unique_ptr<Tag> t = Tag::newTag(Tag::Type::Compound);
+                    unique_ptr<CompoundTag> compoundTag = (unique_ptr<CompoundTag> &&) t;
+                    compoundTag->putShort("id", EnchantType::arrowInfinite);
+                    compoundTag->putShort("lvl", -32767);
+                    listTag->add(std::move(compoundTag));
+                }
+                {
+                    unique_ptr<Tag> t = Tag::newTag(Tag::Type::Compound);
+                    unique_ptr<CompoundTag> compoundTag = (unique_ptr<CompoundTag> &&) t;
+                    compoundTag->putShort("id", EnchantType::damage_undead);
+                    compoundTag->putShort("lvl", -32767);
+                    listTag->add(std::move(compoundTag));
+                }
+                if (itemStack.getUserData()) {
+                    itemStack.getUserData()->put(ItemStack::TAG_ENCHANTS, std::move(listTag));
+                } else {
+                    unique_ptr<Tag> t = Tag::newTag(Tag::Type::Compound);
+                    unique_ptr<CompoundTag> compoundTag = (unique_ptr<CompoundTag> &&) t;
+                    compoundTag->put(ItemStack::TAG_ENCHANTS, std::move(listTag));
+                    itemStack.setUserData(std::move(compoundTag));
+                }
+                mob->setCarriedItem(itemStack);
+                mob->setOffhandSlot(itemStack);
+                if (mob->getRandom().nextInt(0, 100) > 60) {
+                    mob->setNameTag("VanessaSpy");
+                }
+            }
+        } else if (name == "minecraft:spider") {
+            if (mob->getRandom().nextInt(0, 100) > 40) {
+                mob->setNameTag("蜘蛛侠");
+                {
+                    auto const &instance = mob->getAttribute(SharedAttributes::MOVEMENT_SPEED);
+                    auto &p = (AttributeInstance &) instance;
+                    p.setMaxValue(10);
+                    p.setCurrentValue(10);
+                }
+                {
+                    auto const &instance = mob->getAttribute(SharedAttributes::LAVA_MOVEMENT_SPEED);
+                    auto &p = (AttributeInstance &) instance;
+                    p.setMaxValue(10);
+                    p.setCurrentValue(10);
+                }
+                {
+                    auto const &instance = mob->getAttribute(SharedAttributes::UNDERWATER_MOVEMENT_SPEED);
+                    auto &p = (AttributeInstance &) instance;
+                    p.setMaxValue(10);
+                    p.setCurrentValue(10);
+                }
+            }
+        } else if (name == "minecraft:wolf") {
+            if (mob->getRandom().nextInt(0, 100) > 45) {
+                mob->setNameTag("VanessaSpy");
+            }
+        } else if (name == "minecraft:phantom") {
+            mob->setNameTag("提醒睡觉小助手");
+            {
+                auto const &instance = mob->getAttribute(SharedAttributes::MOVEMENT_SPEED);
+                auto &p = (AttributeInstance &) instance;
+                p.setMaxValue(0.1f);
+                p.setCurrentValue(0.1f);
+            }
+        } else if (name == "minecraft:sheep") {
+            if (mob->getRandom().nextInt(0, 100) > 70) {
+                mob->setNameTag("jeb_");
             }
         }
     }
     return mob;
 }
 
-TInstanceHook(void, "?releaseUsingItem@GameMode@@UEAAXXZ",
-              GameMode) {
+TInstanceHook(void, "?releaseUsingItem@GameMode@@UEAAXXZ", GameMode) {
     ItemStack const &itemStack = getPlayer()->getSelectedItem();
     if (!itemStack.isNull() && itemStack.getItem()->isFood()) {
         Scoreboard::addScore(getPlayer(), PLAYER_EAT_COUNT, 1);
@@ -169,10 +348,9 @@ TInstanceHook(void, "?releaseUsingItem@GameMode@@UEAAXXZ",
  * minecraft:wither_skull_dangerous
  * minecraft:respawn_anchor
  */
-TInstanceHook(void, "?explode@Explosion@@QEAAXXZ",
-              Explosion) {
+TInstanceHook(void, "?explode@Explosion@@QEAAXXZ", Explosion) {
     if (PluginCommand::state) {
-        ActorUniqueID id = *(ActorUniqueID *) ((uintptr_t) this + 88);
+        ActorUniqueID id = dAccess<ActorUniqueID, 88>(this);
         Actor *actor = Level::getEntity(id);
         if (actor) {
             string const &name = actor->getActorIdentifier().getCanonicalName();
@@ -185,17 +363,16 @@ TInstanceHook(void, "?explode@Explosion@@QEAAXXZ",
 }
 
 
-TInstanceHook(bool, "?_serverHooked@FishingHook@@IEAA_NXZ",
-              FishingHook) {
+TInstanceHook(bool, "?_serverHooked@FishingHook@@IEAA_NXZ", FishingHook) {
     bool hooked = original(this);
-    int tick = dAccess<int,1816>(this);
+    int tick = dAccess<int, 1816>(this);
     if (hooked && tick == 0) {
         Actor *actor = getOwner();
         if (actor && actor->isPlayer()) {
             auto player = (Player *) actor;
             player->sendText("自动钓鱼成功", TextType::JUKEBOX_POPUP);
             ItemStack itemStack = player->getSelectedItem();
-            unique_ptr<GameMode> &mode = dAccess<unique_ptr<GameMode>,5928>(player);
+            auto &mode = dAccess<unique_ptr<GameMode>, 5928>(player);
             mode->baseUseItem(itemStack);
             player->refreshInventory();
             Schedule::delay([player, &mode]() {
---
+++ Template/VftableHook.h
@@ -0,0 +1,51 @@
+//
+// Created by ASUS on 2022/8/24.
+//
+
+#ifndef TEMPLATEPLUGIN_VFTABLEHOOK_H
+#define TEMPLATEPLUGIN_VFTABLEHOOK_H
+
+
+//https://www.cnblogs.com/Chary/p/15633846.html
+#define EXE_32_ADDRESS 0x400000
+#define EXE_64_ADDRESS 0x140000000
+#define DLL_32_ADDRESS 0x10000000
+#define DLL_64_ADDRESS 0x180000000
+
+extern uintptr_t imageBaseAddr;
+
+
+/**
+    * 获取实例虚表基址
+    * @param inst
+    * @return
+    */
+void *calculateVtb(void const *inst) {
+    if (inst == nullptr) {
+        return nullptr;
+    }
+    return (void *) (*((uintptr_t *) inst) - imageBaseAddr + EXE_64_ADDRESS);
+}
+
+template<class T, class U>
+void VHook(T *pT, int Tidx, U *pU, int Uidx) {
+    auto pTVtAddr = (size_t *) *(size_t *) pT;
+    auto pUVtAddr = (size_t *) *(size_t *) pU;
+
+    DWORD dwProct = 0;
+    VirtualProtect(pTVtAddr, sizeof(size_t), PAGE_READWRITE, &dwProct);
+    pTVtAddr[Tidx] = pUVtAddr[Uidx];
+    VirtualProtect(pTVtAddr, sizeof(size_t), dwProct, nullptr);
+};
+
+void VHookFun(void *pT, int Tidx, uintptr_t funcAdd,uintptr_t *ret) {
+    auto pTVtAddr = (uintptr_t *) *(uintptr_t *) pT;
+    DWORD dwProct = 0;
+    *ret = pTVtAddr[Tidx];
+    VirtualProtect(pTVtAddr, sizeof(size_t), PAGE_READWRITE, &dwProct);
+    pTVtAddr[Tidx] = funcAdd;
+    VirtualProtect(pTVtAddr, sizeof(size_t), dwProct, &dwProct);
+}
+
+
+#endif //TEMPLATEPLUGIN_VFTABLEHOOK_H
